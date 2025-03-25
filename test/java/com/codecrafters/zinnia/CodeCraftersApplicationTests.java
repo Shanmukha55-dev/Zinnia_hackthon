d8f7c2a80e418a968b1259b563a98d0af79b13dd
Shanmukha55-dev <shanmukhakpk@gmail.com>
2025-03-25 22:54:56 +0530

    Add files via upload

---
+++ test/java/com/codecrafters/zinnia/CodeCraftersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrafters.zinnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCraftersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
